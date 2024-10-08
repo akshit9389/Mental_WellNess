62dceb584b021cb1a4fe5a503f5963d929d10b13
akshit9389 <akshitpp01@gmail.com>
2024-10-08 23:11:22 +0530

    nav-completed

--- Landing Page/assets/script.js
+++ Landing Page/assets/script.js
@@ -21,3 +21,30 @@ function loadingAnimation1() {
 }
 
 loadingAnimation1();
+
+let icon = document.querySelector("#ic");
+let navb = document.querySelector(".main-nav");
+let page1 = document.querySelector(".page1");
+
+icon.addEventListener("click", () => {
+  if (navb.style.display === "none" || navb.style.display === "") {
+    navb.style.display = "flex";
+    setTimeout(() => {
+      navb.style.opacity = "1";
+      navb.style.transform = "translateY(0)";
+      page1.style.filter = "blur(8px)";
+    }, 10); // Slight delay to allow display to take effect before animating
+  } else {
+    navb.style.opacity = "0";
+    navb.style.transform = "translateY(-20px)";
+    setTimeout(() => {
+      navb.style.display = "none";
+    }, 500);
+    page1.style.filter = "none";
+  }
+});
+
+
+
+
+
